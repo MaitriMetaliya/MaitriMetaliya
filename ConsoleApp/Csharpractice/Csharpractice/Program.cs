014eea554e445884a138b2585373996f7c524d40
MaitriMetaliya <maitri.m@naapbooks.com>
2024-12-12 05:21:50 -0800

    Add files via upload

---
+++ ConsoleApp/Csharpractice/Csharpractice/Program.cs
@@ -0,0 +1,36 @@
+﻿abstract class Animal
+{
+    public abstract void Makesound();
+
+    public void sleep()
+    {
+        Console.WriteLine("Animal is sleeping");
+    }
+
+}
+class Dog : Animal
+{
+    public override void Makesound()
+    {
+        Console.WriteLine("Woof!");
+    }
+}
+class Cat : Animal
+{
+  
+    public override void Makesound()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Animal dog = new Dog();
+        Animal cat = new Cat();
+        dog.Makesound();
+        cat.Makesound();
+    }
+}
---
+++ ConsoleApp/Csharpractice/Params.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace AccessSpecifiers
+{
+    class Program
+    {
+        public void Show(params int[] val)
+        {
+            for(int i = 0; i < val.Length; i++)
+            {
+                Console.WriteLine(val[i]);
+            }
+        }
+        static void Main(string[] args) { 
+        Program program = new Program();
+            program.Show(1,2,3,4);
+        
+        }
+    }
+}
+
