a3de7ae7cdc44d5ff329633134dabe9c15e55ede
MaitriMetaliya <maitri.m@naapbooks.com>
2024-12-30 19:05:02 +0530

    Update Task 2 employee table.sql

--- MySql/Task 2 employee table.sql
+++ MySql/Task 2 employee table.sql
@@ -8,3 +8,24 @@ begin
 end $$
 DELIMITER ;
 
+#Leap year
+DELIMITER $$
+Create function is_leap_year(year INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    IF (year % 4 = 0 AND (year % 100 != 0 OR year % 400 = 0)) THEN
+        RETURN 1; 
+    ELSE
+        RETURN 0; 
+    END IF;
+END$$
+DELIMITER ;
+
+
+#sp
+DELIMITER $$
+Create Procedure get_all_salary( salary INT)
+begin
+
+end
